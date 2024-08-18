234eab9ca39c00cbc128a16d5d2da3eff35a06e7
Bekarys <bekarys20050812@gmail.com>
2024-08-18 17:26:25 +0500

    finish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Response, Form, Depends
+from fastapi import FastAPI, Response, Form, Depends, Cookie
 from attrs import define, asdict
 from fastapi.security import OAuth2PasswordBearer
 import jwt
-
+import json
 
 app = FastAPI()
 
@@ -23,6 +23,7 @@ class Flower:
   id: int
   name: str
   count: int
+  price: int
 
 class UserRepository:
   def __init__(self):
@@ -109,8 +110,36 @@ async def get_flowers():
 
   
 @app.post('/add_flower')
-async def add_flower(name: str, count: int):
-  flw = Flower(id = 0, name=name , count=count)
+async def add_flower(name: str, count: int, price: int):
+  flw = Flower(id = 0, name=name , count=count, price=price)
   repo_flowers.save(flw)
   return flw.id
 
+@app.post('/cart/items')
+async def add_flower(response: Response, flower_id: int = Form(), items: str = Cookie(default='[]'), price: int = Cookie(default=0)):
+  items_json = json.loads(items)
+  flowers = repo_flowers.get_all()
+
+
+  if flower_id > 0:
+    items_json.append(asdict(flowers[flower_id-1]))
+    price += asdict(flowers[flower_id-1])['price']
+    new_items = json.dumps(items_json)
+    response.set_cookie(key='items', value=new_items)
+    response.set_cookie(key='price', value=price)
+  return response.status_code
+
+@app.get('/cart/items')
+async def get_cart(response: Response, items: str = Cookie(default='[]'), price: int = Cookie(default=0)):
+  items_json = json.loads(items)
+
+  return {
+    'items': items_json,
+    'price': price
+  }
+
+
+@app.delete('/cart/delete_items')
+async def delete_items(response: Response):
+  response.delete_cookie(key='items')
+  return response.status_code
